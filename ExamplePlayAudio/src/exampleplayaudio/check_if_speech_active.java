22c225640ebfb6fc295e1c88484f64350af1644e
tsoglani <mrtsoglanakos@hotmail.com>
2016-10-23 14:34:45 +0300

    sample

--- ExamplePlayAudio/src/exampleplayaudio/check_if_speech_active.java
+++ ExamplePlayAudio/src/exampleplayaudio/check_if_speech_active.java
@@ -1,3 +1,56 @@
+
+    void capture() {
+   
+try { 
+    AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+        TargetDataLine microphone;
+//        SourceDataLine speakers;
+ 
+        microphone = AudioSystem.getTargetDataLine(format);
+
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+        microphone = (TargetDataLine) AudioSystem.getLine(info);
+        microphone.open(format);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int numBytesRead;
+        int CHUNK_SIZE = 1024;
+        byte[] data = new byte[microphone.getBufferSize() / 5];
+        microphone.start();
+
+        
+        
+        
+
+        int bytesRead = 0;
+        DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
+//        speakers = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+//        speakers.open(format);
+//        speakers.start();
+        while (bytesRead < 100000) {
+            numBytesRead = microphone.read(data, 0, CHUNK_SIZE);
+            bytesRead += numBytesRead;
+            
+            
+            
+            /// check if soumeone is talking ...
+            //if is talking       out.write(data, 0, numBytesRead); 
+ // if not talking save to file 
+            
+            // write the mic data to a stream for use later
+            out.write(data, 0, numBytesRead); 
+            // write mic data to stream for immediate playback
+//            speakers.write(data, 0, numBytesRead);
+        }
+//        speakers.drain();
+//        speakers.close();
+        microphone.close();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
+
+
  private void isSpeechActive() {
 
         new Thread() {
